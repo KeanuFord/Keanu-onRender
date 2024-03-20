24b7f00de4d3e9f3780068d3893d1d020567f030
KeanuFord <KeanuF2017@gmail.com>
2024-03-20 14:19:32 -0700

    another message

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 app.get("*",(req,res)=>{
     res.send(`Keanu says: Yay! a ${req.method} request to ${req.url}`)
+    res.send(`Look! A second message!`)
 })
 
 app.listen(PORT,()=>{
